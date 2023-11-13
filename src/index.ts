095014154782e6c565d908369f44ca0184bc1165
João Darwin <joaodarwin.ip22@gmail.com>
2023-11-13 14:57:06 -0300

    feat: Criar base do projeto

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import express, { Request, Response } from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/test", (req: Request, res: Response) => {
+    return res.send("Hello World");
+})
+
+const portApplication = process.env.PORT;
+
+app.listen(portApplication, () => {
+    console.log(`Application running on the port ${portApplication}`);
+})
