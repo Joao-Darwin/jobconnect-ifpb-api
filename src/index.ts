91ef69798653a5f179d56154f598637e4d026616
João Darwin <joaodarwin.ip22@gmail.com>
2023-11-25 20:48:22 -0300

    refactor: Alterar consoles.log pro uso do winston

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { config } from "dotenv";
 import app from "./app";
+import Logger from "../config/logger";
 
 config();
 
 const portApplication: string | undefined = process.env.PORT;
 
 app.listen(portApplication, () => {
-    console.log(`Application running on the port ${portApplication}`);
+    Logger.info(`Application running on the port ${portApplication}`);
 })
