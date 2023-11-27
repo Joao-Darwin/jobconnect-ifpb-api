c27c29853ec1c6f9141a6b5fddb2cc2aa062c0aa
João Darwin <joaodarwin.ip22@gmail.com>
2023-11-27 17:18:10 -0300

    feat: Criar rota padrão no app pra recursos de students

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import instituicaoRoutes from "../router/Instituicao";
+import discenteRoutes from "../router/Discente";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/institutions", instituicaoRoutes);
+app.use("/api/v1/students", discenteRoutes);
 
 export default app;
