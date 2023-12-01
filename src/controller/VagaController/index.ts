64468efdcf6bd5cc97298fc816bd5a2aec740511
João Darwin <joaodarwin.ip22@gmail.com>
2023-12-01 13:10:46 -0300

    feat: Criar findById pra entity Vaga

--- src/controller/VagaController/index.ts
+++ src/controller/VagaController/index.ts
@@ -48,7 +48,48 @@ const findAll = async (req: Request, res: Response) => {
     }
 }
 
+const findById = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const vancancy = await Vagas.findFirst({
+            where: {
+                id: id
+            },
+            select: {
+                id: true,
+                descricao: true,
+                perfilProfissional: true,
+                procedimento: true,
+                created_at: true,
+                updated_at: true,
+                discentes: {
+                    select: {
+                        _count: true
+                    }
+                },
+                empresa: {
+                    select: {
+                        nome: true,
+                        email: true
+                    }
+                }
+
+            }
+        })
+
+        if(vancancy) {
+           return res.send(vancancy);
+        }
+
+        return res.status(404).json(`Vaga dont foud. Id: ${id}`);
+    } catch (error: any) {
+        Logger.error(error.message);
+        res.status(500).json(error.message);
+    }
+}
+
 export default {
     create,
-    findAll
+    findAll,
+    findById
 }
--- src/router/Vaga/index.ts
+++ src/router/Vaga/index.ts
@@ -5,5 +5,6 @@ const vagaRoutes = Router();
 
 vagaRoutes.post("/save", VagaController.create);
 vagaRoutes.get("/", VagaController.findAll);
+vagaRoutes.get("/:id", VagaController.findById);
 
 export default vagaRoutes
